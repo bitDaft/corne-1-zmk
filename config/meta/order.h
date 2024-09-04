0b9af839afef420b5b96238f37bd30a861fed54a
Tausif ali <ajaxhis@tutanota.com>
2024-09-04 21:31:06 +0530

    remove kbdctrl layer

--- config/meta/order.h
+++ config/meta/order.h
@@ -5,12 +5,11 @@
 #include "../layers/nav.layer"
 #include "../layers/num.layer"
 #include "../layers/numpad.layer"
-#include "../layers/kbdctrl.layer"
 
 COLEMAKDH_LAYOUT
 NAV_LAYOUT
 NUM_LAYOUT
 NUMPAD_LAYOUT
-KBDCTRL_LAYOUT
+
 
 #endif // LAYER_ORDER
