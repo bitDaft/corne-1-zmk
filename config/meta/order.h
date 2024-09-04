190056a5984890203754e13ffa6398fc70e059ee
Tausif ali <ajaxhis@tutanota.com>
2024-09-04 20:41:52 +0530

    layout changes

---
+++ config/meta/order.h
@@ -0,0 +1,16 @@
+#ifndef LAYER_ORDER
+#define LAYER_ORDER
+
+#include "../layers/colemakdh.layer"
+#include "../layers/nav.layer"
+#include "../layers/num.layer"
+#include "../layers/numpad.layer"
+#include "../layers/kbdctrl.layer"
+
+COLEMAKDH_LAYOUT
+NAV_LAYOUT
+NUM_LAYOUT
+NUMPAD_LAYOUT
+KBDCTRL_LAYOUT
+
+#endif // LAYER_ORDER
